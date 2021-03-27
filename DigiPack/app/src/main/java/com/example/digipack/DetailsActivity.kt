58de9eaad7b1912b5eff5e5d8abbccca858603b3
Kristine <kh2336@nau.edu>
2021-03-27 13:42:25 -0700

    ver 4 of the mobile app
    Changed the package name, and some UI changes.

---
+++ DigiPack/app/src/main/java/com/example/digipack/DetailsActivity.kt
@@ -0,0 +1,162 @@
+package com.example.digipack
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.synthetic.main.activity_details.*
+import org.json.JSONException
+import org.json.JSONObject
+
+class DetailsActivity : AppCompatActivity() {
+
+    // Call the network detector tool
+    private val networkMonitor = networkDetectorTool(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+
+        val googleId = intent.getStringExtra("google_id")
+        val googleFirstName = intent.getStringExtra("google_first_name")
+        val googleLastName = intent.getStringExtra("google_last_name")
+        val googleEmail = intent.getStringExtra("google_email")
+        val googleProfilePicURL = intent.getStringExtra("google_profile_pic_url")
+        val googleAccessToken = intent.getStringExtra("google_id_token")
+
+        google_first_name_textview.text = googleFirstName
+        //google_email_textview.text = googleEmail
+
+        // Calls the network detector class
+        networkMonitor.result = { isAvailable, type ->
+            runOnUiThread {
+                when (isAvailable) {
+                    true -> {
+                        when (type) {
+                            ConnectionType.Wifi -> {
+                                clouds.setImageResource(R.drawable.sun_connection)
+                                //internet_connection.text = "Wifi Connection"
+                                connectToServer()
+                            }
+                            ConnectionType.Cellular -> {
+                                clouds.setImageResource(R.drawable.sun_connection)
+                                //internet_connection.text = "Cellular Connection"
+                                connectToServer()
+                            }
+                            else -> { }
+                        }
+                    }
+                    false -> {
+                        clouds.setImageResource(R.drawable.networkclouds)
+                        //internet_connection.text = "No Connection"
+                        connectToServer()
+                    }
+                }
+            }
+        }
+
+        // Button that goes to the google classroom page
+        var gclassIntent = Intent(this, gClassActivity::class.java)
+        val gclassButton = findViewById<Button>(R.id.google_classroom_btn)
+        gclassButton.setOnClickListener(){
+            this.startActivity(gclassIntent)
+        }
+
+        // Button that goes to the google search page
+        var gsearchIntent = Intent(this, gSearchActivity::class.java)
+        val gsearchButton = findViewById<Button>(R.id.google_search_btn)
+        gsearchButton.setOnClickListener(){
+            this.startActivity(gsearchIntent)
+        }
+
+        // Automatically calls connection to the server
+        connectToServer()
+    }
+
+
+    private fun connectToServer(){
+        val googleId = intent.getStringExtra("google_id")
+        val googleFirstName = intent.getStringExtra("google_first_name")
+        val googleLastName = intent.getStringExtra("google_last_name")
+        val googleEmail = intent.getStringExtra("google_email")
+        val googleProfilePicURL = intent.getStringExtra("google_profile_pic_url")
+        val googleAccessToken = intent.getStringExtra("google_id_token")
+
+        val mptv = findViewById<TextView>(R.id.mptext)
+        val authurl = "auth/"
+        val getlisturl = "user/$googleEmail"
+        val queue = RequestQueueSingleton.getInstance(this.applicationContext)
+        val tok = JsauthTok(googleAccessToken, googleEmail)
+        val gtok = Gson().toJson(tok)
+        val jsobtok = JSONObject(gtok)
+        var resp = JSONObject("{Result:noACK}")
+
+        val request = JsonObjectRequest(Request.Method.POST, getString(R.string.serverUrl).plus(authurl), jsobtok,
+            { response -> resp = response
+                if( resp.get("Result") == "ACK"){
+                    val success = "Authentication Successful"
+                    mptv.text = success
+
+                    val user = Jsuser(googleFirstName, googleEmail, googleId)
+                    val guser = Gson().toJson(user)
+                    val jsuserobj = JSONObject(guser)
+                    var filelistResp = JSONObject("{Result:noACK}")
+
+                    val filelistRequest = JsonObjectRequest(Request.Method.GET, getString(R.string.serverUrl).plus(getlisturl), jsuserobj,
+                        { flresponse -> filelistResp = flresponse
+                            try{
+                                Log.i(getString(R.string.app_name), "in details act, %s".format(flresponse.toString()))
+                                var flintent = Intent(this, FileListViewActivity::class.java)
+                                flintent.putExtra("fileListJson", flresponse.toString())
+                                flintent.putExtra("gsoData", intent.extras)
+                                val nextbtn = findViewById<Button>(R.id.btn_pick)
+                                nextbtn.setOnClickListener(){
+                                    this.startActivity(flintent)
+                                }
+                            }catch(e: JSONException){
+                                Log.e(getString(R.string.app_name), "JSON key error: %s".format(e))
+                            }
+                        },
+                        { err -> Log.i(getString(R.string.app_name), err.toString())
+                            Toast.makeText(applicationContext, err.toString(), Toast.LENGTH_LONG).show()}
+                    )
+                    queue.addToRequestQueue(filelistRequest)
+                }
+            },
+            { error ->
+                mptv.text = error.toString() }
+        )
+        queue.addToRequestQueue(request)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        networkMonitor.register()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        networkMonitor.unregister()
+    }
+}
+
+data class Jsuser(
+    @SerializedName("userName")
+    var userName: String? = null,
+    @SerializedName("googleEmail")
+    var email: String? = null,
+    @SerializedName("googleId")
+    var gid: String? = null
+)
+
+data class JsauthTok (
+    @SerializedName("googleAccessToken")
+    var authToken: String? = null,
+    @SerializedName("gooogleEmail")
+    var email: String? = null
+)
---
+++ DigiPack/app/src/main/java/com/example/digipack/networkDetectorTool.kt
@@ -0,0 +1,91 @@
+package com.example.digipack
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.os.Build
+
+
+enum class ConnectionType {
+    Wifi, Cellular
+}
+
+class networkDetectorTool(context: Context) {
+
+    private var networkContext = context
+    private lateinit var networkCallback: NetworkCallback
+    lateinit var result: ((isAvailable: Boolean, type: ConnectionType?) -> Unit)
+
+    @Suppress("DEPRECATION")
+    fun register() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val connectivityManager = networkContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if (connectivityManager.activeNetwork == null) {
+                result(false,null)
+            }
+
+            // Check if the type of connection have changed
+            networkCallback = object : NetworkCallback() {
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    result(false, null)
+                }
+
+                override fun onCapabilitiesChanged(network: Network, networkCapabilities: NetworkCapabilities) {
+                    super.onCapabilitiesChanged(network, networkCapabilities)
+                    when {
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                            result(true,ConnectionType.Wifi)
+                        }
+                        else -> {
+                            result(true,ConnectionType.Cellular)
+                        }
+                    }
+                }
+            }
+            connectivityManager.registerDefaultNetworkCallback(networkCallback)
+        } else {
+            // Use Intent Filter for Android 8 and below
+            val intentFilter = IntentFilter()
+            intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
+            networkContext.registerReceiver(networkChangeReceiver, intentFilter)
+        }
+    }
+
+    fun unregister() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val connectivityManager =
+                networkContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            connectivityManager.unregisterNetworkCallback(networkCallback)
+        } else {
+            networkContext.unregisterReceiver(networkChangeReceiver)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private val networkChangeReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+
+            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            // Get Type of Connection
+            if (activeNetworkInfo != null) {
+                when (activeNetworkInfo.type) {
+                    ConnectivityManager.TYPE_WIFI -> {
+                        result(true, ConnectionType.Wifi)
+                    }
+                    else -> {
+                        result(true, ConnectionType.Cellular)
+                    }
+                }
+            } else {
+                result(false, null)
+            }
+        }
+    }
+}
